7c8b53f16f05dd2c250196bbc93c0d81273deba7
Lluís Revilla <lluis.revilla_sancho@roche.com>
2026-03-13 12:39:42 +0100

    Fix documentation issues and use checkmate to check better

--- R/assertion.R
+++ R/assertion.R
@@ -1,8 +1,9 @@
-
 #' Assert level
 #'
 #' @param x `picks` object
 #' @param class Class of the last element of picks
+#' @inheritParams checkmate::makeAssertionFunction
+#' @inheritParams checkmate::assert
 #' @rdname assert_level
 #' @returns For `check_level` a logical value or a string.
 #' For `assert_level` invisibly the object checked or an error.
@@ -12,7 +13,7 @@
 #' x <- picks(datasets(), variables(), values())
 #' assert_level(x, "values")
 check_level <- function(x, class) {
-  assertthat::is.string(class)
+  checkmate::assert_character(class, len = 1, any.missing = FALSE)
   check <- inherits(x, "picks") && inherits(x[[length(x)]], class)
   if (isFALSE(check)) {
     return(sprintf("This is not a picks object that ends in %s", class))
