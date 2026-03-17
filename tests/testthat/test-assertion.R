5e3f5a632cffd1cd505bc0acac198bd37951d8ae
Lluís Revilla <lluis.revilla_sancho@roche.com>
2026-03-17 11:40:00 +0100

    Update tests

--- tests/testthat/test-assertion.R
+++ tests/testthat/test-assertion.R
@@ -1,16 +1,16 @@
-testthat::describe("check_level works", {
+testthat::describe("check_last_level works", {
   testthat::test_that("checks last picks", {
     x <- list(datasets(), variables(), values())
-    testthat::expect_match(check_level(x, "values"), "picks object")
+    testthat::expect_match(check_last_level(x, "values"), "picks object")
   })
 
   testthat::test_that("checks last picks", {
     x <- picks(datasets(), variables(), values())
-    testthat::expect_true(check_level(x, "values"))
+    testthat::expect_true(check_last_level(x, "values"))
   })
 
   testthat::test_that("returns FALSE", {
     x <- picks(datasets(), variables(), values())
-    testthat::expect_match(check_level(x, "variables"), "in variables")
+    testthat::expect_match(check_last_level(x, "variables"), "in variables")
   })
 })
