43eb88e346e2e28c3cc4fe38adc96b692b9f030f
André Veríssimo <211358+averissimo@users.noreply.github.com>
2026-03-23 17:40:48 +0000

    Adds assertions and better defaults to pick (#31)
    # Pull Request
    
    - fixes #28 
    
    ### Changes description
    
    - Improved conversion that accounts for arguments in `variables()` that
    cannot take `NULL`
    - Adds assertions
    - Hints `checkmate` to which variables assertions fails on
    `checkmate::assert()`
    
    ---------
    
    Co-authored-by: Lluís Revilla <185338939+llrs-roche@users.noreply.github.com>

--- R/0-as_picks.R
+++ R/0-as_picks.R
@@ -211,7 +211,7 @@ teal_transform_filter <- function(x, label = "Filter") {
 
 .select_spec_to_variables <- function(x) {
   if (length(x)) {
-    variables(
+    args <- list(
       choices = if (inherits(x$choices, "delayed_data")) {
         out <- x$choices$subset
         if (is.null(out)) {
@@ -242,6 +242,10 @@ teal_transform_filter <- function(x, label = "Filter") {
       multiple = x$multiple,
       fixed = x$fixed
     )
+    if (is.null(args$ordered)) { # Must be logical or missing for variables() to set default value
+      args <- args[names(args) != c("ordered")]
+    }
+    do.call(variables, args)
   }
 }
 
