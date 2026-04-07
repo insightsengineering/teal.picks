c39d28793c6d24814fff98625b9ae5c170cb44a8
Lluís Revilla <lluis.revilla_sancho@roche.com>
2026-04-07 17:12:16 +0200

    Protect against missing testthat (suggested) dependency

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,6 +1,9 @@
 # This file is part of the standard {testthat} test
 # temporary directory, which is the default).
-library(testthat)
-library(teal.picks)
+if (requireNamespace("testthat", quietly = TRUE)) {
+  library(testthat)
+  library(teal.picks)
 
-test_check("teal.picks")
+  test_check("teal.picks")
+
+}
