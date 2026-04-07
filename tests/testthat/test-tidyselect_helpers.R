add5268ab9d7f51996124f0348c8d69b796c9693
Lluís Revilla <lluis.revilla_sancho@roche.com>
2026-04-07 17:12:33 +0200

    Remove more prefixes

--- tests/testthat/test-tidyselect_helpers.R
+++ tests/testthat/test-tidyselect_helpers.R
@@ -1,31 +1,31 @@
 describe("ranged assertions", {
   it("accepts min max to be numeric", {
-    testthat::expect_no_error(ranged(0L, 10L))
-    testthat::expect_no_error(ranged(0.0, 10.0))
+    expect_no_error(ranged(0L, 10L))
+    expect_no_error(ranged(0.0, 10.0))
   })
 
   it("accepts min max to be infinite", {
-    testthat::expect_no_error(ranged(-Inf, Inf))
+    expect_no_error(ranged(-Inf, Inf))
   })
 
   it("doesn't accept missing values", {
-    testthat::expect_error(ranged(NA, Inf))
-    testthat::expect_error(ranged(-Inf, NA))
+    expect_error(ranged(NA, Inf))
+    expect_error(ranged(-Inf, NA))
   })
 
   it("throws when min/max are not scalars", {
-    testthat::expect_error(ranged(c(1, 2), Inf))
-    testthat::expect_error(ranged(-Inf, c(1, 2)))
+    expect_error(ranged(c(1, 2), Inf))
+    expect_error(ranged(-Inf, c(1, 2)))
   })
 
   it("doesn't accept min to be larger than max", {
-    testthat::expect_error(ranged(1, 0))
+    expect_error(ranged(1, 0))
   })
 
   it("returns a function of x and ...", {
     out <- ranged(0, 10)
-    testthat::expect_true(is.function(out))
-    testthat::expect_identical(names(formals(out)), "x")
+    expect_true(is.function(out))
+    expect_identical(names(formals(out)), "x")
   })
 })
 
@@ -37,8 +37,8 @@ describe("ranged when resolved", {
       values(choices = ranged(2, 9), selected = ranged(4L, 6L))
     )
     out <- resolver(data = list(df = data.frame(a = seq_len(10))), x = picks_unresolved)
-    testthat::expect_s3_class(out$values$choices, "ranged")
-    testthat::expect_s3_class(out$values$selected, "ranged")
+    expect_s3_class(out$values$choices, "ranged")
+    expect_s3_class(out$values$selected, "ranged")
   })
 
   it("resolved choices contains original values which match specified range", {
@@ -48,7 +48,7 @@ describe("ranged when resolved", {
       values(choices = ranged(2, 9), selected = ranged(4L, 6L))
     )
     out <- resolver(data = list(df = data.frame(a = seq_len(10))), x = picks_unresolved)
-    testthat::expect_identical(unclass(out$values$choices), seq(2, 9))
+    expect_identical(unclass(out$values$choices), seq(2, 9))
   })
 
   it("returns selected range containing values from vector based on the specified range", {
@@ -58,7 +58,7 @@ describe("ranged when resolved", {
       values(choices = ranged(), selected = ranged(4L, 6L))
     )
     out <- resolver(data = list(df = data.frame(a = seq_len(10))), x = picks_unresolved)
-    testthat::expect_identical(unclass(out$values$selected), c(4L, 6L))
+    expect_identical(unclass(out$values$selected), c(4L, 6L))
   })
 
   it("returns selected range containing shrinked range to existing range-values", {
@@ -68,7 +68,7 @@ describe("ranged when resolved", {
       values(choices = ranged(), selected = ranged(4, 6))
     )
     out <- resolver(data = list(df = data.frame(a = c(1, 2, 4.3, 5.7, 6.00001))), x = picks_unresolved)
-    testthat::expect_identical(unclass(out$values$selected), c(4.3, 5.7))
+    expect_identical(unclass(out$values$selected), c(4.3, 5.7))
   })
 
   it("fails when range is outside any value", {
