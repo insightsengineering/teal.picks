2eaa18405c466605c42100ba5d4d64cb881672fc
André Veríssimo <211358+averissimo@users.noreply.github.com>
2026-04-14 13:21:58 +0100

    docs: adds documentation

--- R/helpers.R
+++ R/helpers.R
@@ -1,36 +1,47 @@
-#' Check if a pick has the multiple attribute set to TRUE
+#' Helper functions for pick
+#' @description
+#' Hleper functions for pick objects generated from
+#' `datasets()`, `variables()` or `values()`:
+#' @name helper_functions_pick
 #' @param x (`datasets`, `variables` or `values`) pick to check.
-#' @return `TRUE` if the pick has the `multiple` attribute set to `TRUE`,
+#' @return `TRUE` if the pick has the attribute set to `TRUE`,
 #' `FALSE` otherwise.
+
+
+#' @rdname helper_functions_pick
+#' @description
+#' - `is_picks_multiple()` checks if a pick has the `multiple` attribute set to `TRUE`.
 #' @examples
-#' p <- picks(datasets("iris"), variables())
+#' p <- picks(datasets("iris"), variables(), values())
+#'
 #' is_pick_multiple(p$variables)
+#' @export
 is_pick_multiple <- function(x) {
   checkmate::assert_multi_class(x, classes = c("pick"))
   checkmate::assert_flag(attr(x, "multiple", exact = TRUE))
   isTRUE(attr(x, "multiple", exact = TRUE))
 }
 
-#' Check if a pick has the fixed attribute set to TRUE
-#' @param x (`datasets`, `variables` or `values`) pick to check.
-#' @return `TRUE` if the pick has the `fixed` attribute set to `
-#' TRUE`, `FALSE` otherwise.
+#' @rdname helper_functions_pick
+#' @description
+#' - `is_pick_fixed()` checks if a pick has the `fixed` attribute set to `TRUE`.
 #' @examples
-#' p <- picks(datasets("iris"), variables())
+#'
 #' is_pick_fixed(p$variables)
+#' @export
 is_pick_fixed <- function(x) {
   checkmate::assert_multi_class(x, classes = c("pick"))
   checkmate::assert_flag(attr(x, "fixed", exact = TRUE))
   isTRUE(attr(x, "fixed", exact = TRUE))
 }
 
-#' Check if a pick has the ordered attribute set to TRUE
-#' @param x (`datasets`, `variables` or `values`) pick to check.
-#' @return `TRUE` if the pick has the `ordered` attribute set to `
-#' TRUE`, `FALSE` otherwise.
+#' @rdname helper_functions_pick
+#' @description
+#' - `is_pick_ordered()` checks if a pick has the `ordered` attribute set to `TRUE`.
 #' @examples
-#' p <- picks(datasets("iris"), variables())
+#'
 #' is_pick_ordered(p$variables)
+#' @export
 is_pick_ordered <- function(x) {
   checkmate::assert_multi_class(x, classes = c("pick"))
   checkmate::assert_flag(attr(x, "ordered", exact = TRUE))
