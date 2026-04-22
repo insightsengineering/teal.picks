14f6d23e0824aba2e8e93ed3140bafae57354929
André Veríssimo <211358+averissimo@users.noreply.github.com>
2026-04-22 14:55:18 +0100

    docs: minor change in docs to be a bit more explicit

--- R/0-picks.R
+++ R/0-picks.R
@@ -13,9 +13,9 @@
 #' @param fixed (`logical(1)`) selection will be fixed and not possible to change interactively.
 #' @param ordered (`logical(1)`) if the selected should follow the selection order. If `FALSE`
 #'   `selected` returned from `srv_module_input()` would be ordered according to order in `choices`.
-#' @param ... for `picks()`: hierarchical structure that contains `datasets()` as first element and optionally `variables()` and `values()`
+#' @param ... for `picks(...)`: hierarchical structure that contains `datasets()` as first element and optionally `variables()` and `values()`
 #'
-#' for `variables()` and `values()`: additional arguments delivered to `pickerInput`
+#' for `variables(...)` and `values(...)`: additional arguments delivered to `pickerInput`
 #' @param dataset_check: (`logical(1)`) whether to check that the first element of `picks` is `datasets()`.
 #' This is useful to set to `FALSE` when creating picks objects that have a required dataset that is not selected by the user and defined in the module itself.
 #' @details
