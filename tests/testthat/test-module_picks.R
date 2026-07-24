5c0e7066d93fe40a23365b428639663457105cf8
Lluís Revilla <lluis.revilla_sancho@roche.com>
2026-07-24 17:09:37 +0200

    Test also with reactivity

--- tests/testthat/test-module_picks.R
+++ tests/testthat/test-module_picks.R
@@ -949,7 +949,10 @@ testthat::describe("picks_srv resolves picks interactively", {
   })
 
   it("adding a dataset to data adds new choice to dataset choices", {
-    skip("todo: tests can't trigger data()")
+
+    shiny::reactiveConsole(TRUE)
+    on.exit(reactiveConsole(FALSE))
+
     test_picks <- picks(
       datasets(choices = tidyselect::everything(), selected = 1L),
       variables(choices = tidyselect::everything(), selected = 1L)
@@ -984,7 +987,9 @@ testthat::describe("picks_srv resolves picks interactively", {
   })
 
   it("adding a column to data adds new choice to variables-choices", {
-    skip("todo: tests can't trigger data()")
+
+    shiny::reactiveConsole(TRUE)
+    on.exit(reactiveConsole(FALSE))
     test_picks <- picks(
       datasets(choices = tidyselect::everything(), selected = 1L),
       variables(choices = tidyselect::everything(), selected = 1L)
