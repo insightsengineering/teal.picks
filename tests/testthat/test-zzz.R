b644a6dfdba5a60f17e5cab86f20833f6f834848
André Veríssimo <211358+averissimo@users.noreply.github.com>
2026-08-28 17:58:41 +0100

    fix: test

--- tests/testthat/test-zzz.R
+++ tests/testthat/test-zzz.R
@@ -3,13 +3,6 @@ test_that(".onLoad registers teal.picks logger", {
   with_mocked_bindings(
     log_success = function(...) succeed(),
     .package = "logger",
-    expect_success(.onLoad(libname = "", pkgname = "teal.picks"))
+    expect_success(.onLoad(libname = "dummy_lib", pkgname = "teal.picks"))
   )
-})
-
-test_that(".onload registers teal.picks handlers", {
-  testthat::skip_if(getOption("testthat_interactive"))
-  .onLoad(libname = "", pkgname = "teal.picks")
-  registered_handlers_namespaces <- getFromNamespace("registered_handlers_namespaces", "teal.logger")
-  expect_equal(registered_handlers_namespaces[["teal.picks"]], "teal.picks")
 })
