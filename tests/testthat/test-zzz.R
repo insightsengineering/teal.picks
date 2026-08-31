4cc6d579cf1550aa5e29a6878c9ad23370a7752a
Lluís Revilla <lluis.revilla_sancho@roche.com>
2026-08-31 10:24:02 +0200

    Check no error is generated

--- tests/testthat/test-zzz.R
+++ tests/testthat/test-zzz.R
@@ -1,8 +1,4 @@
 test_that(".onLoad registers teal.picks logger", {
   testthat::skip_if(getOption("testthat_interactive"))
-  with_mocked_bindings(
-    log_success = function(...) succeed(),
-    .package = "logger",
-    expect_success(.onLoad(libname = "dummy_lib", pkgname = "teal.picks"))
-  )
+  expect_no_error(.onLoad(libname = "dummy_lib", pkgname = "teal.picks"))
 })
