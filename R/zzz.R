bacab555dc8da23bbbe85b581da34cbda65168b0
Lluís Revilla <lluis.revilla_sancho@roche.com>
2026-08-31 09:24:19 +0200

    Make registration more portable

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onLoad <- function(libname, pkgname) { # nolint
   # Set up the teal logger instance
-  teal.logger::register_logger("teal.picks")
-  teal.logger::register_handlers("teal.picks")
+  teal.logger::register_logger(pkgname)
+  teal.logger::register_handlers(pkgname)
 
   # Manual import instead of using backports and adding 1 more dependency
   if (getRversion() < "4.4") {
