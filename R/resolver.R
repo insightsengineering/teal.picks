c53fd978be3ebfba1bdbb5d357593d5fb7bf8a5e
Lluís Revilla <lluis.revilla_sancho@roche.com>
2026-09-16 13:32:27 +0200

    Fix style

--- R/resolver.R
+++ R/resolver.R
@@ -254,10 +254,11 @@ determine.values <- function(x, data) {
   if (length(out) == 0) {
     warningCondition(
       paste0(
-      "None of the `choices/selected`: ", rlang::as_label(x), "\n",
-      "are subset of: ", toString(.possible_choices(data), width = 30), "\n",
-      "Emptying choices...",
-      if (is.character(x)) "Setting explicit requirements might be the cause."),
+        "None of the `choices/selected`: ", rlang::as_label(x), "\n",
+        "are subset of: ", toString(.possible_choices(data), width = 30), "\n",
+        "Emptying choices...",
+        if (is.character(x)) "Setting explicit requirements might be the cause."
+      ),
       class = c("pick_delayed", "picks_delayed")
     )
     return(NULL)
